ad01631dfc65719850505f58dfaa8487fef90ce9
Luis Resendiz <80994311+luisrdevy@users.noreply.github.com>
2021-05-12 09:07:40 -0500

    Add files via upload

---
+++ p4-client.c
@@ -0,0 +1,45 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+int main() {
+
+    unsigned char buffer[512] = "como vamos gerry :v";
+
+    int udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if(udp_socket==-1) {
+        printf("Open socket: failed\n");
+        return 1;
+    }
+
+    printf("Open socket: OK\n");
+
+    // Client configuration
+
+    struct sockaddr_in client;
+
+    client.sin_family = AF_INET;
+    client.sin_port = htons(8080);
+    client.sin_addr.s_addr = inet_addr("10.0.2.15");
+
+    // Send message
+    
+    int sendto_status = sendto(udp_socket, buffer, strlen(buffer) + 1, 0, (struct sockaddr *) &client, sizeof(client));
+
+    if (sendto_status == -1) {
+        printf("Send message: failed\n");
+        return 1;
+    }
+
+    printf("Send message: OK\n");
+
+    close(udp_socket);
+
+    return 0;
+}
---
+++ p5-server.c
@@ -0,0 +1,57 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+int main() {
+
+    // Server cconfiguration
+
+    int udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if(udp_socket == -1) {
+        printf("Open socket: failed\n");
+        return 1;
+    }
+
+    printf("Open socket: OK\n");
+
+    struct sockaddr_in server;
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(8080);
+    server.sin_addr.s_addr = INADDR_ANY;
+
+    int bind_status = bind(udp_socket, (struct sockaddr *) &server, sizeof(server));
+
+    if(bind_status == -1) {
+        printf("Bind: failed");
+        return 1;
+    }
+
+    printf("Bind: OK\n");
+
+
+    // Get client message
+
+    struct sockaddr_in client;
+    unsigned char buffer[512];
+
+    int client_size = sizeof(client);
+    int recv_status = recvfrom(udp_socket, buffer, 512, 0, (struct sockaddr *) &client, &client_size);
+
+    if (recv_status == -1) {
+        printf("Get message: failed\n");
+        return 1;
+    }
+
+    printf("Client message: %s\n", buffer);
+
+    close(udp_socket);
+
+    return 0;
+}
